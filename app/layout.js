b441959362a15e6f37dc7b7fbaad7d57e60345a9
Jakub Pałka <jakub.palka97@gmail.com>
2026-03-30 10:24:40 +0200

    clarity

--- app/layout.js
+++ app/layout.js
@@ -147,6 +147,13 @@ export default function RootLayout({ children }) {
 						gtag('config', 'G-BHHCJCQTRE');
 					`}
 				</Script>
+				<script type="text/javascript">
+    (function(c,l,a,r,i,t,y){
+        c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "w3rmch45b7");
+</script>
 			</head>
 			<body className={`${inter.variable} ${montserrat.variable} antialiased bg-white text-slate-900`}>{children}</body>
 		</html>
